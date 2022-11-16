3562d1071cb26314c05a4717c8715516ea861b9c
Mukit Bin Momin <mukitb@amazon.com>
2022-11-16 13:40:28 -0500

    Update to version v1.7.0

--- source/bin/streaming-data-solution.ts
+++ source/bin/streaming-data-solution.ts
@@ -1,22 +1,25 @@
 #!/usr/bin/env node
+
 import 'source-map-support/register';
+
 import * as cdk from '@aws-cdk/core';
-import { AwsSdkConfig } from './aws-sdk-aspect';
-import { CfnNagAspect } from './cfn-nag-aspect';
-import crypto = require('crypto');
 
 import { ApiGwKdsLambda } from '../patterns/apigw-kds-lambda';
-import { KplKdsKda } from '../patterns/kpl-kds-kda';
-import { KdsKdfS3 } from '../patterns/kds-kdf-s3';
+import { AppRegistry } from '../lib/app-registry';
+import { AwsSdkConfig } from './aws-sdk-aspect';
+import { CfnNagAspect } from './cfn-nag-aspect';
 import { KdsKdaApiGw } from '../patterns/kds-kda-apigw';
-import { MskStandalone } from '../patterns/msk-standalone-cluster';
-import { MskLambda } from '../patterns/msk-lambda';
-import { MskLambdaKdf } from '../patterns/msk-lambda-kdf';
-import { MskKdaS3 } from '../patterns/msk-kda-s3';
-
-import { MskLambdaRoleStack } from '../labs/msk-lambda-role';
+import { KdsKdfS3 } from '../patterns/kds-kdf-s3';
+import { KplKdsKda } from '../patterns/kpl-kds-kda';
 import { MskClientStack } from '../labs/msk-client-setup';
 import { MskClusterStack } from '../labs/msk-cluster-setup';
+import { MskKdaS3 } from '../patterns/msk-kda-s3';
+import { MskLambda } from '../patterns/msk-lambda';
+import { MskLambdaKdf } from '../patterns/msk-lambda-kdf';
+import { MskLambdaRoleStack } from '../labs/msk-lambda-role';
+import { MskStandalone } from '../patterns/msk-standalone-cluster';
+
+import crypto = require('crypto');
 
 const app = new cdk.App();
 const solutionIdKds = 'SO0124';
@@ -28,6 +31,11 @@ function applyAspects(stacks: cdk.Stack[], solutionId: string) {
         const hash = crypto.createHash('sha256').update(stack.stackName).digest('hex');
         cdk.Aspects.of(stack).add(new AwsSdkConfig(app, `CustomUserAgent-${hash}`, solutionId));
         cdk.Aspects.of(stack).add(new CfnNagAspect(app, `CfnNag-${hash}`));
+        cdk.Aspects.of(stack).add(
+            new AppRegistry(stack, `AppRegistry-${hash}`, {
+                solutionID: solutionId
+            })
+        );
     }
 }
 
