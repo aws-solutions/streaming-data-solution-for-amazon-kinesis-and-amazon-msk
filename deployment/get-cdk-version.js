8388d4c1cfe0579ad09424951782d374a53bac7e
Morris Estepa <estepamo@amazon.com>
2024-10-17 10:28:37 -0500

    Update to version v1.9.5

--- deployment/get-cdk-version.js
+++ deployment/get-cdk-version.js
@@ -1,2 +1,6 @@
+/** ************************************************************************************************
+*   Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.                             *
+*   SPDX-License-Identifier: Apache-2.0                                                            *
+ ************************************************************************************************ */
 const cdkPackage = require('../source/node_modules/aws-cdk/package.json');
 console.log(cdkPackage.version);
